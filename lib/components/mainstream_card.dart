60b5c207606baa40f09893e1192cc2638a101721
kyun04 <efrencamposagrado0622@gmail.com>
2024-09-15 08:51:47 +0800

    first commit with emojis ui

--- lib/components/mainstream_card.dart
+++ lib/components/mainstream_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pagesapp/components/emoji.dart';
 import 'package:pagesapp/components/mainstream_user_profile.dart';
 
 class MainstreamCard extends StatefulWidget {
@@ -15,7 +17,8 @@ class _MainstreamCardState extends State<MainstreamCard> {
     return Container(
       width: MediaQuery.of(context).size.width,
       child: Container(
-          margin: const EdgeInsets.only(left: 8, top: 20, bottom: 10),
+          margin:
+              const EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 5),
           decoration: BoxDecoration(
               boxShadow: [
                 BoxShadow(
@@ -47,7 +50,32 @@ class _MainstreamCardState extends State<MainstreamCard> {
                         child: Image.asset('assets/images/catnip1.png',
                             width: MediaQuery.of(context).size.width,
                             fit: BoxFit.cover)),
-                  )
+                  ),
+                  Container(
+                      child: Row(children: [
+                    Emoji(
+                      imageAssetUrl: 'assets/svg/red-heart-svgrepo-com.svg',
+                      emojiCount: '4',
+                    ),
+                    Emoji(
+                      imageAssetUrl:
+                          'assets/svg/celebration-day-fireworks-svgrepo-com.svg',
+                      emojiCount: '3',
+                    ),
+                    Emoji(
+                      imageAssetUrl: 'assets/svg/happy-emoji-svgrepo-com.svg',
+                      emojiCount: '1',
+                    ),
+                    Emoji(
+                      imageAssetUrl: 'assets/svg/light-bulb-svgrepo-com.svg',
+                      emojiCount: '0',
+                    ),
+                    Emoji(
+                      imageAssetUrl:
+                          'assets/svg/sad-but-relieved-face-svgrepo-com.svg',
+                      emojiCount: '0',
+                    )
+                  ]))
                 ])
               ]))),
     );
